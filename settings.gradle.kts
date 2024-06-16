7566012f141ef8120902b0a7a46c1f5c5d3689d4
MrE8065 <102242666+MrE8065@users.noreply.github.com>
2024-06-16 15:29:07 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
-rootProject.name = "CloudstreamPlugins"
+rootProject.name = "NubePlugins"
 
 // This file sets what projects are included. Every time you add a new project, you must add it
 // to the includes below.
 
 // Plugins are included like this
 include(
-    "NubePlugins"
+    "AnimeFLVProvider"
 )

3116e72335bbb914be7578196884af342a1c2aeb
MrE8065 <102242666+MrE8065@users.noreply.github.com>
2026-05-04 19:15:21 +0200

    Typo

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:9.1.1")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
     }
 }
 

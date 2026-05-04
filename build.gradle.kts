8eacd38242acec45478fe153ebb2649d5f4604e2
MrE8065 <102242666+MrE8065@users.noreply.github.com>
2026-05-04 19:14:57 +0200

    Update updated stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.android.tools.build:gradle:9.1.1")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
